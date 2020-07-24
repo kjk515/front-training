c9fe1d279065aaf4be5e27b7b19d88310ac21b88
강진경 <jkkang@wemakeprice.com>
2020-07-24 19:00:39 +0900

    element ui 추가

--- week1/src/main.js
+++ week1/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ko'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { locale })
 
 new Vue({
   render: function (h) { return h(App) },
